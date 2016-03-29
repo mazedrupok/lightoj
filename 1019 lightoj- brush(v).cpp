8fc91bcd462bac7c61ac937905a2843ed714704c
Md. Abdul Mazed Rupok <mazed4g@gmail.com>
2016-03-29 23:02:40 +0600

    Create 1019 lightoj- brush(v).cpp

---
+++ 1019 lightoj- brush(v).cpp
@@ -0,0 +1,56 @@
+#include <bits//stdc++.h>
+using namespace std;
+#define inf 2147483647
+vector <int> G[105];
+vector <int> cost[105];
+int dis[105];
+struct node {
+   int city; int dist;
+   node (int a, int b) {city = a; dist = b;}
+   bool operator < (const node &p) const {
+      return dist < p.dist; //large to small..
+   }
+};
+int dijkstra (int src, int n) {
+   for (int i = 0; i < 105; i++) dis[i] = inf;
+   priority_queue <node > pq;
+   dis[src] = 0;
+   pq.push(node(src, 0));
+   while (!pq.empty()) {
+      node a = pq.top(); pq.pop();
+      int u = a.city;
+
+      int len = G[u].size();
+      for (int i = 0; i < len; i++) {
+         int v = G[u][i];
+         if (dis[v] > dis[u] + cost[u][i]) {
+            dis[v] = dis[u] + cost[u][i];
+            pq.push (node (v, dis[v]));
+         }
+      }
+   }
+   return dis[n];
+}
+int main ()
+{
+   int tc, cas = 0, u, v, w, e, n;
+   freopen ("in.txt", "r", stdin);
+   scanf (" %d", &tc);
+   while (tc--) {
+      scanf (" %d %d", &n, &e);
+      for (int i = 0; i < e; i++) {
+         scanf (" %d %d %d", &u, &v, &w);
+         G[u].push_back (v);
+         G[v].push_back (u);
+         cost[u].push_back (w);
+         cost[v].push_back (w);
+      }
+
+      int res = dijkstra(1, n);
+      if (res == inf) printf ("Case %d: Impossible\n", ++cas);
+      else printf ("Case %d: %d\n", ++cas, res);
+
+      vector <int > t1[105], t2[105];
+      swap (G, t1); swap (cost, t2);
+   }
+}
